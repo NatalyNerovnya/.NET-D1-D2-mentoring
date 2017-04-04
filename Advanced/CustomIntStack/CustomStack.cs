e77b96eec443cfb99fe59e4ca34f63e7c45eafcd
NatalliaN <Natallia.Nerovnya@easyJet.com>
2017-04-04 18:39:10 +0300

    Collections and generics 3 task with bad Equals

---
+++ Advanced/CustomIntStack/CustomStack.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomIntStack
+{
+    public class CustomStack : IEnumerable<int>, IEquatable<CustomStack>
+    {
+        private int size;
+        private int capacity;
+        private static readonly int defaultCapacity = 8;
+        private int[] stack;
+
+        public int Count
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public CustomStack() : this(defaultCapacity) { }
+
+        public CustomStack(int initialCapacity)
+        {
+            capacity = initialCapacity;
+            size = 0;
+            stack = new int[capacity];
+        }
+
+        public CustomStack(params int[] collection)
+            : this(collection.Length)
+        {
+            stack = collection;
+            size = collection.Length;
+        }
+
+        public void Push(int element)
+        {
+            if (size + 1 > capacity)
+                ExtendCapacity();
+            stack[size++] = element;
+        }
+
+        public int Pop()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+            var result = stack[size - 1];
+            stack[--size] = 0;
+            return result;
+        }
+
+        public int Peek()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+            return stack[size - 1];
+
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (stack[i] == element)
+                    return true;
+            }
+            return false;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (stack[i] < 0)
+                    yield break;
+                yield return stack[i];
+            }
+        }
+
+        //very bad!! Don't use foreach loop because it will stop after first negative integer
+        public bool Equals(CustomStack other)
+        {
+            if (other == null)
+                return false;
+            if (other.Count != this.Count)
+                return false;
+
+            int i = this.size - 1;
+            foreach (var element in other)
+            {
+                if (i < 0)
+                    break;
+                if (element != this.stack[i--])
+                    return false;
+            }
+            return true;
+        }
+
+        private void ExtendCapacity()
+        {
+            capacity *= 2;
+            var newStack = new int[capacity];
+            for (int i = 0; i < size; i++)
+            {
+                newStack[i] = stack[i];
+            }
+
+            stack = newStack;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Advanced/CustomIntStackUnitTests/CustomStackTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomIntStack;
+
+namespace CustomIntStackUnitTests
+{
+    [TestClass]
+    public class CustomStackTests
+    {
+        [TestMethod]
+        public void Count_StackOfFiveElements_Five()
+        {
+            var actual = new CustomStack(1,2,3,4,5);
+            int expected = 5;
+
+            int result = actual.Count;
+
+            Assert.AreEqual(expected,result);
+        }
+
+        [TestMethod]
+        public void Count_StackOfZeroElements_Zero()
+        {
+            var actual = new CustomStack();
+            int expected = 0;
+
+            int result = actual.Count;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Push_StackOfThreeElements_StackOfFourElements()
+        {
+            var actual = new CustomStack(1,2,3);
+            var expected = new CustomStack(1,2,3,4);
+
+            actual.Push(4);
+
+            Assert.AreEqual(actual.Count, expected.Count);
+            Assert.AreEqual(actual.Equals(expected), true);
+        }
+
+        [TestMethod]
+        public void Pop_ThreeTwoOneStack_Three()
+        {
+            var actual = new CustomStack(1,2,3);
+
+            var result = actual.Pop();
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Peek_ThreeTwoOneStack_Three()
+        {
+            var actual = new CustomStack(1, 2, 3);
+
+            var result = actual.Peek();
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Contains_OneTwoThreeFourFiveContainThree_True()
+        {
+            var actual = new CustomStack(1,2,3,4,5);
+
+            var result = actual.Contains(3);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void Contains_OneTwoThreeFourFiveContainTen_False()
+        {
+            var actual = new CustomStack(1, 2, 3, 4, 5);
+
+            var result = actual.Contains(10);
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void GetEnumerator_OneTwoMinusThreeFourFive_FiveFour()
+        {
+            var actual = new CustomStack(1,2,-3,4,5);
+            var expected = new []{5,4,0,0,0};
+
+            var result = new int[5];
+            int i = 0;
+            foreach (var element in actual)
+            {
+                result[i++] = element;
+            }
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+    }
+}
