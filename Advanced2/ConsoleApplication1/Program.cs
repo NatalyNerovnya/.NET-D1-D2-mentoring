db90ae3680560b93cef4928d21ac1938a83ba028
NatalliaN <Natallia.Nerovnya@easyJet.com>
2017-04-09 12:23:16 +0300

    add iterator

---
+++ Advanced2/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FileSystem;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @"C:\";
+            var fVisitr = new FileSystemVisitor(@"D:\");
+            foreach (var file in fVisitr)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Advanced2/FileSystem/FileSystemVisitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystem
+{
+    public class FileSystemVisitor : IEnumerable<string>
+    {
+        private string path; 
+
+        public FileSystemVisitor() : this(@"C:\") { }
+
+        public FileSystemVisitor(string path)
+        {
+            if(ReferenceEquals(path, null))
+                throw new ArgumentNullException();
+            this.path = path;
+        }
+
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            if (Directory.Exists(path))
+            {
+                var dirs = Directory.GetDirectories(path);
+                foreach (var s in dirs)
+                {
+                    yield return s;
+                }
+                var files = Directory.GetFiles(path);
+                foreach (var s in files)
+                {
+                    yield return s;
+                }
+            }
+            else
+            {
+                yield return "Such path doesn't exist!";
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
