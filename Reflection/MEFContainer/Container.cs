4ec914ad094d7b4af68e3355fc18f22d8a1d2133
Nataly Nerovnya <natanerovnja@gmail.com>
2017-05-01 15:24:48 +0300

    Register/Resolve methods

---
+++ Reflection/MEFContainer/Container.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MEFContainer
+{
+    public class Container
+    {
+        private readonly Dictionary<Type, Type> mapper;
+
+        public Container()
+        {
+            mapper = new Dictionary<Type, Type>();
+        }
+
+        public void Register<TKey,TValue>()
+        {
+            mapper.Add(typeof(TKey), typeof(TValue));
+        }
+
+        public T Resolve<T>()
+        {
+            Type type = mapper[typeof(T)];
+            return (T)Resolve(type);
+        }
+
+        private object Resolve(Type type)
+        {
+            var resolvedType = mapper[type];
+            var ctorInfo = resolvedType.GetConstructors().First();
+            var parameters = ctorInfo.GetParameters();
+
+            if (!parameters.Any())
+            {
+                return Activator.CreateInstance(resolvedType);
+            }
+            else
+            {
+                return ctorInfo.Invoke(ResolveParameters(parameters).ToArray());
+            }
+        }
+
+        private IEnumerable<object> ResolveParameters(IEnumerable<ParameterInfo> parameters)
+        {
+            return parameters.Select(p => Resolve(p.ParameterType)).ToList();
+        }
+    }
+}
