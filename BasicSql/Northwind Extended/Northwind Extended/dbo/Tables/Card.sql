aae1e0a3353de77fae3f1956662c6a3229bd8d2b
NatalliaN <Natallia_Nerovnya@epam.com>
2017-05-19 16:36:17 +0300

    Add task 3.2

---
+++ BasicSql/Northwind Extended/Northwind Extended/dbo/Tables/Card.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Card]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CardNumber] INT NOT NULL, 
+    [ExpireDate] DATE NOT NULL, 
+    [CardHolder] NVARCHAR(50) NOT NULL, 
+    [CustomerId] NCHAR(5) NOT NULL, 
+    CONSTRAINT [FK_Card_ToCustomers] FOREIGN KEY ([CustomerID]) REFERENCES [Customers]([CustomerID])
+)
