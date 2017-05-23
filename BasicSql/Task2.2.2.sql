49ce8aa8442ae846d1521ffb024deadee6f9ed31
NatalliaN <Natallia_Nerovnya@epam.com>
2017-05-23 10:01:23 +0300

    Small sql fixes

--- BasicSql/Task2.2.2.sql
+++ BasicSql/Task2.2.2.sql
@@ -1,11 +1,6 @@
---По таблице Orders найти количество заказов, cделанных каждым продавцом. Заказ для указанного продавца – это любая запись в 
---таблице Orders, где в колонке EmployeeID задано значение для данного продавца. В результатах запроса надо возвращать колонку 
---с именем продавца (Должно высвечиваться имя полученное конкатенацией LastName & FirstName. Эта строка LastName & FirstName должна 
---быть получена отдельным запросом в колонке основного запроса. Также основной запрос должен использовать группировку по EmployeeID.) 
---с названием колонки ‘Seller’ и колонку c количеством заказов возвращать с названием 'Amount'. Результаты запроса должны быть 
---упорядочены по убыванию количества заказов.
-
-SELECT EmployeeID AS [Seller], COUNT(CustomerID) AS [Amount]
-FROM [Northwind].[dbo].[Orders]
-GROUP BY EmployeeID
+SELECT o.EmployeeID AS [Seller], COUNT(CustomerID) AS [Amount] , CONCAT(e.LastName ,e.FirstName ) AS [Name]
+FROM Orders o
+JOIN Employees  e
+ON o.EmployeeID = e.EmployeeID
+GROUP BY o.EmployeeID, CONCAT(e.LastName ,e.FirstName ) 
 ORDER BY [Amount] DESC
