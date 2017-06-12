e5d1ab1089149ea24191c6f4c42dd9bf33acde92
NatalliaN <Natallia_Nerovnya@epam.com>
2017-06-12 15:32:46 +0300

    Add reports

--- Logging/MvcMusicStore/Global.asax.cs
+++ Logging/MvcMusicStore/Global.asax.cs
@@ -1,8 +1,12 @@
 ﻿namespace MvcMusicStore
 {
+    using System.Configuration;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using System.Web.WebPages;
+
+    using Logging;
 
     using MvcMusicStore.Infrastructure;
 
@@ -10,6 +14,15 @@
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        private ILogger logger;
+        public MvcApplication()
+        {
+            bool enableLogging = ConfigurationManager.AppSettings["Logging"].AsBool();
+            if (enableLogging)
+            {
+                this.logger = new Logger();
+            }
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -21,6 +34,13 @@
             {
                 counterHelper.RawValue(Counters.Login, 0);
             }
+        }
+
+        protected void Application_Error()
+        {
+            var error = Server.GetLastError();
+            this.logger?.LogError(error, error.Message);
+            this.logger?.LogError(error, error.Message);
         }
     }
 }
