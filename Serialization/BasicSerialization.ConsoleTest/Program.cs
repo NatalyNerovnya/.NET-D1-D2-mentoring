5761878879c7d8905065ef35b9820c7447b3546c
NatalliaN <Natallia_Nerovnya@epam.com>
2017-06-21 13:27:33 +0300

    Base for serialization task

---
+++ Serialization/BasicSerialization.ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BasicSerialization.ConsoleTest
+{
+    using System.IO;
+    using System.Xml.Serialization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "../../App_Data/books.xml";
+            var serializer = new XmlSerializer(typeof(Catalog));
+            var catalog = serializer.Deserialize(
+         new FileStream(filePath, FileMode.Open)) as Catalog;
+
+        }
+    }
+}
---
+++ Serialization/BasicSerialization/Catalog.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicSerialization
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [Serializable]
+    [XmlType(TypeName = "catalog")]
+    [XmlRoot(Namespace = "http://library.by/catalog")]
+    public class Catalog
+    {
+        //[XmlArrayItem("book")]
+        //[XmlElement("book")]
+        //public IEnumerable<Book> Books { get; set; }
+
+        [XmlAttribute("date")]
+        public DateTime Date { get; set; }
+    }
+}
