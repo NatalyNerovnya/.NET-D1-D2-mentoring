d145f5e1509103c80fd391a92dafc1c4987b7af8
NatalliaN <Natallia_Nerovnya@epam.com>
2017-07-06 14:08:36 +0300

    Task 2

---
+++ Patterns/Task2/ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿namespace Task2
+{
+    using System.Collections.Concurrent;
+
+    public class ObjectPool<T> where T : class
+    {
+        private readonly ConcurrentBag<T> container = new ConcurrentBag<T>();
+
+        private readonly IPoolObjectCreator<T> poolObjectCreator;
+
+        public int Count { get { return this.container.Count; } }
+
+        public ObjectPool(IPoolObjectCreator<T> poolObject)
+        {
+            this.poolObjectCreator = poolObject;
+        }
+
+        public T GetObject()
+        {
+            T obj;
+            if (this.container.TryTake(out obj))
+            {
+                return obj;
+            }
+
+            return this.poolObjectCreator.Create();
+        }
+
+        public void ReturnObject(ref T obj)
+        {
+            this.container.Add(obj);
+            obj = null;
+        }
+    }
+}
