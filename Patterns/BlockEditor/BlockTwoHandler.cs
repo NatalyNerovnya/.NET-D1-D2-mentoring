172ffe908a63a2b6145f3d763362405de89f1f30
NatalliaN <Natallia_Nerovnya@epam.com>
2017-07-07 18:32:24 +0300

    Task 3

---
+++ Patterns/BlockEditor/BlockTwoHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace BlockEditor
+{
+    using System.Diagnostics;
+
+    public class BlockTwoHandler : IHandler
+    {
+        public IHandler Successor { get; set; }
+
+        public void HandleRequestAction()
+        {
+            Debug.WriteLine("Do some block2 action(e.g show text)");
+            if (true)
+            {
+                Debug.WriteLine("Transfer responsibility to next block");
+            }
+        }
+    }
+}
